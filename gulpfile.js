8480bb6add268fd9ce364dc9e2588a76a0f2fe48
Greg Smith <elmnt.public@gmail.com>
2018-08-17 23:04:30 -0700

    Kirby Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@
 
 // ----------- Dependencies
 
-var gulp    = require('gulp'),
+var gulp = require('gulp'),
     bsrSync = require('browser-sync').create(),
-    concat  = require('gulp-concat'),
-    uglify  = require('gulp-uglify'),
-    rename  = require('gulp-rename'),
-    sass    = require('gulp-sass'),
-    maps    = require('gulp-sourcemaps'),
-    cache   = require('gulp-cache'),
-    del     = require('del'),
-    runseq  = require('run-sequence');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    runseq = require('run-sequence');
 
 // ----------- Variables
 
@@ -28,9 +28,9 @@ var ks_s = 'site/snippets/*.php',
 var kc = 'content/**/*.txt';
 
 // src assets
-var src_js     = 'src/js/',
+var src_js = 'src/js/',
     src_js_all = 'src/js/*',
-    src_s      = 'src/stringer/**/*.scss';
+    src_s = 'src/stringer/**/*.scss';
 
 // ----------- Static Server & Watch Files
 
@@ -54,51 +54,51 @@ gulp.task('serve', ['compileSass'], function() {
 // ----------- Concat & Minify JS
 
 gulp.task('concatScripts', function() {
-  return gulp.src([
-    src_js + 'jquery-2.1.4.min.js',
-    src_js + 'elmenu.js',
-    src_js + 'picturefill.min.js',
-    src_js + 'jquery.easing.1.3.js',
-    src_js + 'main.js'
-  ])
-  .pipe(concat('app.js'))
-  .pipe(gulp.dest(src_js));
+    return gulp.src([
+            src_js + 'jquery-2.1.4.min.js',
+            src_js + 'elmenu.js',
+            src_js + 'picturefill.min.js',
+            src_js + 'jquery.easing.1.3.js',
+            src_js + 'main.js'
+        ])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(src_js));
 });
 
 gulp.task('minifyScripts', ['concatScripts'], function() {
-  return gulp.src(src_js + 'app.js')
-    .pipe(maps.init())
-    .pipe(uglify())
-    .pipe(rename('app.min.js'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest(ka_j));
+    return gulp.src(src_js + 'app.js')
+        .pipe(maps.init())
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest(ka_j));
 });
 
 // ----------- Compile Sass
 
-gulp.task('compileSass', function () {
-  return gulp.src('src/stringer/styles.scss')
-    .pipe(maps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(rename('styles.min.css'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest(ka_c))
-    .pipe(bsrSync.stream());
+gulp.task('compileSass', function() {
+    return gulp.src('src/stringer/styles.scss')
+        .pipe(maps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename('styles.min.css'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest(ka_c))
+        .pipe(bsrSync.stream());
 });
 
 // ----------- Cleanup
 
 gulp.task('clean', function(callback) {
-  del(ka_c + '*.css*');
-  del(ka_j + '*.js*');
-  del(src_js + 'app.js');
-  return cache.clearAll(callback);
+    del(ka_c + '*.css*');
+    del(ka_j + '*.js*');
+    del(src_js + 'app.js');
+    return cache.clearAll(callback);
 })
 
 // ----------- Build
 
 gulp.task('build', function(callback) {
-  runseq('clean', 'compileSass', 'minifyScripts', callback);
+    runseq('clean', 'compileSass', 'minifyScripts', callback);
 });
 
 // ----------- Default (start the local server)
